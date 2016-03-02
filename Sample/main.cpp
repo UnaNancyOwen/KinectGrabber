8d55f5f7c66e89c314cf5d9c32d8aec076b85c9f
Tsukasa Sugiura <t.sugiura0204@gmail.com>
2016-03-02 21:51:53 +0900

    Update Sample

---
+++ Sample/main.cpp
@@ -0,0 +1,59 @@
+// Disable Error C4996 that occur when using Boost.Signals2.
+#ifdef _DEBUG
+#define _SCL_SECURE_NO_WARNINGS
+#endif
+
+#include "kinect_grabber.h"
+#include <pcl/visualization/pcl_visualizer.h>
+
+typedef pcl::PointXYZRGBA PointType;
+
+int main( int argc, char* argv[] )
+{
+    // PCL Visualizer
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer(
+        new pcl::visualization::PCLVisualizer( "Point Cloud Viewer" ) );
+    viewer->setCameraPosition( 0.0, 0.0, -2.5, 0.0, 0.0, 0.0 );
+
+    // Point Cloud
+    pcl::PointCloud<PointType>::ConstPtr cloud;
+
+    // Retrieved Point Cloud Callback Function
+    boost::mutex mutex;
+    boost::function<void( const pcl::PointCloud<PointType>::ConstPtr& )> function =
+        [&cloud, &mutex]( const pcl::PointCloud<PointType>::ConstPtr& ptr ){
+            boost::mutex::scoped_lock lock( mutex );
+            cloud = ptr;
+        };
+
+    // KinectGrabber
+    pcl::Grabber* grabber = new pcl::KinectGrabber();
+
+    // Register Callback Function
+    boost::signals2::connection connection = grabber->registerCallback( function );
+
+    // Start Grabber
+    grabber->start();
+
+    while( !viewer->wasStopped() ){
+        // Update Viewer
+        viewer->spinOnce();
+
+        boost::mutex::scoped_try_lock lock( mutex );
+        if( cloud && lock.owns_lock() ){
+            if( cloud->size() != 0 ){
+                /* Processing to Point Cloud */
+
+                // Update Point Cloud
+                if( !viewer->updatePointCloud( cloud, "cloud" ) ){
+                    viewer->addPointCloud( cloud, "cloud" );
+                }
+            }
+        }
+    }
+
+    // Stop Grabber
+    grabber->stop();
+
+    return 0;
+}
